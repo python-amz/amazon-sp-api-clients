2922136013cb2ecc8e3ddc2fcea47d4f568ef1a9
panhaoyu <panhaoyu.china@outlook.com>
2022-08-15 16:54:40 +0800

    use poetry to manage requirements

--- upload.py
+++ upload.py
@@ -3,14 +3,14 @@ import shutil
 
 
 def main():
-    # shutil.rmtree('dist', ignore_errors=True)
+    shutil.rmtree('dist', ignore_errors=True)
     version = 'patch'  # patch, minor, major, prepatch, preminor, premajor, prerelease
-    # os.system(f'poetry version {version}')
-    # os.system(f'poetry publish --build')
+    os.system(f'poetry version {version}')
+    os.system(f'poetry publish --build')
     os.system(f'git add "pyproject.toml"')
     version_number = os.popen('poetry version').read().split(' ')[1]
     os.system(f'git commit -m "v{version_number}: new {version} version"')
-    os.system('git push origin HEAD')
+    os.system('git push')
 
 
 if __name__ == '__main__':
