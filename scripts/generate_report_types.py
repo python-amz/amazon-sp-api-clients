beb7800479f9fd320813b06ecee361910913d86e
panhaoyu <panhaoyu.china@outlook.com>
2022-09-17 09:55:49 +0800

    Compatibility adjustments to report types

--- scripts/generate_report_types.py
+++ scripts/generate_report_types.py
@@ -5,11 +5,10 @@ from pathlib import Path
 
 import black
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 from jinja2 import Template
 
-from amazon_sp_api_clients import ReportTypeGroup
-from amazon_sp_api_static.report_types import ReportType
+from amazon_sp_api_static.report_types import ReportType, ReportTypeGroup
 
 base_dir = Path(__file__).absolute().parent.parent
 
@@ -100,63 +99,43 @@ class Script:
 
     @cached_property
     def report_enum(self) -> dict[str, ParsedReport]:
-        result: dict[str, ParsedReport] = {
-            i.upload_name: ParsedReport(
-                name=i.upload_name.lower(), group_name=i.group.name, group_index=i.group_index,
-                upload_name=i.upload_name, description='', report_index=i.report_index
-            )
-            for i in ReportType}
-        for group_name, reports in self.parsed_data.items():
-            for report_name, report in reports.items():
-                if report_name in result:
-                    result[report_name].description = report.description
-                else:
-                    report.group_index = self.group_enum[group_name]
-                    reports_of_group = tuple(i for i in result.values() if i.group_index == report.group_index)
-                    report.report_index = max((*(i.report_index for i in reports_of_group), 0)) + 1
-                    print(f'Found new report: {report.name} = {report.group_index}, {report.report_index}')
-                    result.setdefault(report_name, report)
+        result: dict[str, ParsedReport] = {}
+        for i in ReportType:
+            if i.upload_name in result:
+                name = f'{i.upload_name}_deprecated'
+                result[name] = ParsedReport(
+                    name=name.lower(), group_name=i.group.name, group_index=i.group_index,
+                    upload_name=i.upload_name, description='', report_index=i.report_index
+                )
+            else:
+                result[i.upload_name] = ParsedReport(
+                    name=i.upload_name.lower(), group_name=i.group.name, group_index=i.group_index,
+                    upload_name=i.upload_name, description='', report_index=i.report_index
+                )
+            # for group_name, reports in self.parsed_data.items():
+            #     for report_name, report in reports.items():
+            #         if report_name in result:
+            #             result[report_name].description = report.description
+            #         else:
+            #             report.group_index = self.group_enum[group_name]
+            #             reports_of_group = tuple(i for i in result.values() if i.group_index == report.group_index)
+            #             report.report_index = max((*(i.report_index for i in reports_of_group), 0)) + 1
+            #             print(f'Found new report: {report.name} = {report.group_index}, {report.report_index}')
+            #             result.setdefault(report_name, report)
         # noinspection PyTypeChecker
         result = dict(sorted(result.items(), key=lambda i: i[1].group_index * 100 + i[1].report_index))
         return result
 
     def run(self):
-        groups: dict[str, dict[str, str]] = {}
-        for h2_element, *td_elements in self.parsed_data:
-            group_name = h2_element.text.strip().lower() \
-                .replace(' ', '_').replace('(', '').replace(')', '').replace('-', '_')
-            group_name = re.sub(r'_+', '_', group_name)
-            for row in td_elements:
-                row: Tag
-                lines = row.text.splitlines()
-                lines = [i.strip() for i in lines]
-                lines = [i for i in lines if i]
-                match len(lines):
-                    case 1:
-                        line1 = lines[0]
-                        # if there's only one line, then it's like a subtitle, so just skip is ok
-                        if line1 in ('FBA Sales Reports',):
-                            continue
-                        raise NotImplementedError(f'Unsupported line: {line1}')
-                    case 2 | 3 | 4:
-                        line = ' '.join(lines)
-                        assert (
-                            m := re.match(r'^([A-Za-z 0-9_:\-()&]*?) reportType value: ?([A-Z0-9_]+?)$', line)), line
-                        report_name, report_type_slug = m.group(1, 2)
-                    case other:
-                        raise NotImplementedError(f'TD element with {other} lines cannot be parsed')
-                report_name = report_type_slug.lower()
-                groups.setdefault(group_name, {}).setdefault(report_name, report_type_slug)
-
         template_path = base_dir / 'swagger_client_generator' / 'report_types.pyt'
         with open(template_path, 'r', encoding='utf-8') as f:
             template = Template(f.read())
-        content = template.render(groups=groups)
-        content = black.format_str(content, mode=black.Mode(line_length=120))
+        content = template.render(groups=self.group_enum, reports=self.report_enum)
+        content = black.format_str(content, mode=black.Mode(line_length=300))
         content = reduce(lambda i, j: str.replace(i, '\n\n', '\n'), range(5), content)
-        content = black.format_str(content, mode=black.Mode(line_length=120))
-        with open(base_dir / 'amazon_sp_api_static' / 'report_types.py', 'w', encoding='utf-8') as f:
-            f.write(content)
+        content = black.format_str(content, mode=black.Mode(line_length=300))
+        # with open(base_dir / 'amazon_sp_api_static' / 'report_types.py', 'w', encoding='utf-8') as f:
+        #     f.write(content)
         with open(base_dir / 'amazon_sp_api_clients' / 'report_types.py', 'w', encoding='utf-8') as f:
             f.write(content)
 
